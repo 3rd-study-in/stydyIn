da6cd80b09dc6bb9f54d516618b6c8bae1d1a6e1
kimjinx2 <jinjins0629@gmail.com>
2026-02-27 18:32:05 +0900

    chore / App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-// import { MainCategoryIcon } from './shared/components/Category';
-// // 이미지 4번(atoms/Icon/index.js)에서 정의된 모든 아이콘을 가져옵니다.
-// import { 
-//   Special, Learn, Practical, Project, 
-//   Challenge, Certification, Job, More 
-// } from './atoms/Icon';
-
-// function App() {
-
-//   return (
-//     <>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react';
-import AuthSystem from './shared/components/auth/AuthSystem.jsx';
+import { MainCategoryIcon } from './shared/components/Category';
+// 이미지 4번(atoms/Icon/index.js)에서 정의된 모든 아이콘을 가져옵니다.
+import { 
+  Special, Learn, Practical, Project, 
+  Challenge, Certification, Job, More 
+} from './atoms/Icon';
 
 function App() {
+
   return (
-    <div className=" bg-bg font-sans antialiased">
-      <main className="flex ">
-        <AuthSystem />
-      </main>
-    </div>
+    <>
+    </>
   );
 }
 
 export default App;
+
+
