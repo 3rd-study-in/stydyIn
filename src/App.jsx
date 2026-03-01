826b7b5d56ec94faf9fe6d82abb2e09dd10afe8b
kimjinx2 <jinjins0629@gmail.com>
2026-03-02 00:37:24 +0900

    chore/ App.jsx 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-//
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import AuthSystem from './shared/components/Auth/AuthSystem.jsx';
-import ResetPasswordPage from './shared/components/Auth/ResetPasswordPage.jsx';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="bg-bg font-sans antialiased">
-        <main className="flex">
-          {/* <Routes> */}
-          {/* <Route path="/" element={<AuthSystem />} /> */}
-          <ResetPasswordPage></ResetPasswordPage>
-          {/* </Routes> */}
-        </main>
-      </div>
-    </BrowserRouter>
-  );
+  return <div className="App"></div>;
 }
+
 export default App;
--- src/shared/components/auth/ResetPasswordPage.jsx
+++ src/shared/components/auth/ResetPasswordPage.jsx
@@ -166,3 +166,27 @@ const ResetPasswordPage = () => {
 };
 
 export default ResetPasswordPage;
+
+//
+
+// src/App.jsx
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// // import AuthSystem from './shared/components/Auth/AuthSystem.jsx';
+// import ResetPasswordPage from './shared/components/Auth/ResetPasswordPage.jsx';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="bg-bg font-sans antialiased">
+//         <main className="flex">
+//           {/* <Routes> */}
+//           {/* <Route path="/" element={<AuthSystem />} /> */}
+//           <ResetPasswordPage></ResetPasswordPage>
+//           {/* </Routes> */}
+//         </main>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
