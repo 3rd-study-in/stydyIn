09c0290b146007acc8b8716c212e9dccbd43fd1d
kimjinx2 <jinjins0629@gmail.com>
2026-03-03 16:55:28 +0900

    chore/App.jsx 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-import { useState } from "react";
-// 경로에서 Common/을 제거하고 Pagination 폴더로 직접 연결합니다.
-import Pagination from "./shared/components/Pagination"; 
-
 function App() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 10;
-
-  return (
-    <div className="p-10">
-      <Pagination 
-        currentPage={currentPage} 
-        totalPages={totalPages} 
-        onPageChange={(page) => setCurrentPage(page)} 
-      />
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
